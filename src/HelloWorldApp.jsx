7f74b169876cdecd879544c52af6423cedc2906e
hisahito <hisahito706@gmail.com>
2022-06-29 00:29:55 -0500

    Using propTypes for first time, installing proptypes dependencies using vite is necessary

--- src/HelloWorldApp.jsx
+++ src/HelloWorldApp.jsx
@@ -1,10 +1,18 @@
-const App = () => {
+import PropTypes from 'prop-types';
+
+const App = (title, subTitle) => {
+
  return (
    <>
-    <h1>Hola Mundo</h1>
-    <p>Soy un subtitulo</p>
+    <h1>{title}</h1>
+    <p>{subTitle}</p>
    </>
  );
 }
 
 export default App;
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number,
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App />
+        <App title="Hola soy Goku"/>
     </React.StrictMode>
 );
 
